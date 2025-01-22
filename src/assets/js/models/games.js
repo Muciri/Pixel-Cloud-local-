0cb45671f03cfb4c00d5f67d0ce965379d121964
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2025-01-22 08:25:16 -0300

    small fixes

--- src/assets/js/models/games.js
+++ src/assets/js/models/games.js
@@ -3,61 +3,61 @@ const games =
     {
         id: 1,
         nome: 'Blasphemous II',
-        img: 'public/images/games/Blasphemous.png'
+        img: '/images/games/Blasphemous.png'
     },
 
     {
         id: 2,
         nome: 'Castlevania Symphony of The Night',
-        img: 'public/images/games/Castlevania.png'
+        img: '/images/games/Castlevania.png'
     },
 
     {
         id: 3,
         nome: 'Elden Ring',
-        img: 'public/images/games/EldenRing.png' 
+        img: '/images/games/EldenRing.png' 
     },
 
     {
         id: 4,
         nome: 'GTA V',
-        img: 'public/images/games/GTAV.png' 
+        img: '/images/games/GTAV.png' 
     },
 
     {
         id: 5,
         nome: 'Hollow Knight',
-        img: 'public/images/games/HollowKnight.png' 
+        img: '/images/games/HollowKnight.png' 
     },
 
     {
         id: 6,
         nome: 'Metroid Fusion',
-        img: 'public/images/games/MetroidFusion.png' 
+        img: '/images/games/MetroidFusion.png' 
     },
 
     {
         id: 7,
         nome: 'Red Dead Redemption 2',
-        img: 'public/images/games/RD2.png' 
+        img: '/images/games/RD2.png' 
     },
 
     {
         id: 8,
         nome: 'Spider Man 2',
-        img: 'public/images/games/SpiderMan2.png' 
+        img: '/images/games/SpiderMan2.png' 
     },
 
     {
         id: 9,
         nome: 'Street Fighter 6',
-        img: 'public/images/games/StreetFighter.png' 
+        img: '/images/games/StreetFighter.png' 
     },
 
     {
         id: 10,
         nome: 'Terraria',
-        img: 'public/images/games/Terraria.png' 
+        img: '/images/games/Terraria.png' 
     }
 ]
 
